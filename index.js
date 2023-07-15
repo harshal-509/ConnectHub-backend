c15ff25a622d1191d61249ec1deb3e49e8b20af8
harshal-509 <sharma.harshal509@gmail.com>
2023-07-15 17:05:57 +0530

    added Production CORS origin

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(
     cors({
         credentials: true,
-        origin: "https://connecthub-frontend.onrender.com",
+        origin: process.env.CORS_ORIGIN,
     })
 );
 
